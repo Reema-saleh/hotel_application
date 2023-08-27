0289a9f777bf59cba190cb281c28a7be6fc8f5cb
Reema-saleh <108183299+Reema-saleh@users.noreply.github.com>
2023-08-27 03:24:34 +0300

    Update textfield_widget.dart

--- lib/components/textfield_widget.dart
+++ lib/components/textfield_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hotel_application/constants/colors.dart';
 import 'package:hotel_application/constants/spaces.dart';
 
-
 class TextfieldWidget extends StatelessWidget {
   const TextfieldWidget({
     super.key,
@@ -12,12 +11,10 @@ class TextfieldWidget extends StatelessWidget {
     this.suffiIcon,
     this.width = 300,
     this.height = 60,
-
-     this.controller, 
+    this.controller,
     this.textFun,
     this.obscureText = false,
     this.radius,
-
   });
 
   final String label;
@@ -44,27 +41,27 @@ class TextfieldWidget extends StatelessWidget {
         ),
         kHSpace8,
         SizedBox(
-          height: height,
-          width: width,
-            onChanged: textFun,
-            clipBehavior: Clip.hardEdge,
-            obscureText: obscureText,
-            controller: controller,
-            textAlignVertical: TextAlignVertical.center,
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                prefixIcon: preIcon,
-                suffixIcon: suffiIcon,
-                hintText: hintText,
-                hintStyle: const TextStyle(
-                  fontSize: 13,
-                ),
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: AppColors.primary),
-                    borderRadius: BorderRadius.circular(radius ?? 15))),
-          ),
-        ),
+            height: height,
+            width: width,
+            child: TextField(
+              onChanged: textFun,
+              clipBehavior: Clip.hardEdge,
+              obscureText: obscureText,
+              controller: controller,
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: preIcon,
+                  suffixIcon: suffiIcon,
+                  hintText: hintText,
+                  hintStyle: const TextStyle(
+                    fontSize: 13,
+                  ),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: AppColors.primary),
+                      borderRadius: BorderRadius.circular(radius ?? 15))),
+            )),
       ],
     );
   }
